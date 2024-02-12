f5e332644418c261807be95a01267992f4df08a1
Bruno Eneas <brunovn.eneas@gmail.com>
2024-02-11 23:57:31 -0300

    Adicionado as funções de movimento e transformação das blocos.

--- js/script.js
+++ js/script.js
@@ -62,5 +62,73 @@ document.addEventListener('DOMContentLoaded', () =>{
         }, 300);
     }
 
-    
+    function placeRandom(){
+        const avaliable = [];
+        for(let i=0; i<size; i++){
+                for(let j=0; j<size; j++){
+                    if(board[i][j] === 0){
+                        avaliable.push({x: i, y:j});
+                    }
+                }
+        }
+
+        if(avaliable.length > 0){
+            const randomCell = avaliable[Math.floar(Math.random() = avaliable.length)];
+            board[randomCell.x][randomCell.y] = Math.random() < 0.9 ? 2 : 4; 
+            const cell = document.querySelector(`[data-rows="${randomCell.x}"][data-col="${randomCell.j}"]`);
+            cell.classList.add('new-tile');
+        }
+    }
+
+    function move(direction) {
+        let hasChanged = false;
+        if(direction === 'ArrowUp' || direction === 'ArrowDown'){
+            for(let j=0 ; j < size ; j++ ){
+                const column = [...Array(size)].map((_,i) => board[i][j]);
+                const newColumn = transform(column, direction==='ArrowUp');
+                for (let i=0; i < size; i++){
+                    if(board[i][j] !== newColumn[i]){
+                        hasChanged = true;
+                        board[i][j] = newColumn[i];
+                    }
+                }
+            }
+        }else if(direction === 'ArrowLeft' || direction === 'ArrowRight'){
+            for (let i=0; i < size; i++){
+                const row = board[j];
+                const newRow = transform(row, direction==='ArrowLeft');
+                if(row.join(',') !== newRow.join(',')){
+                    hasChanged = true;
+                    board[i] = newRow;
+                }
+            }
+        }
+        if (hasChanged){
+            placeRandom();
+            renderBoard();
+            checkGameOver();
+        }
+    }
+
+    function transform(line, moveTowardStart){
+        let newLine = line.filter(cell => cell !== 0);
+        if (!moveTowardStart){
+            newLine.reverse();
+        }
+        for (let i = 0 ; i < newLine.length - 1 ; i++){
+            if(newLine[i] == newLine[i + 1]){
+                newLine[i] += 2;
+                updateScore(newLine[i]);
+                newLine.splice(i + 1, 1)
+            }
+        }
+        while(newLine.length < size){
+            newLine.push(0)
+        }
+        if(!moveTowardStart){
+            newLine.reverse()
+        }
+        return newLine;
+    }   
+
 })
